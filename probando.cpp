e7371c325d4072c4683162544e5b9d815eb8ef95
Paco Galán <pgalann@hotmail.com>
2021-10-07 11:27:39 +0200

    Desde Visual Code se puso mas bonito

--- probando.cpp
+++ probando.cpp
@@ -7,21 +7,26 @@ int main()
     double x, y, z;
     char c;
     cout << "CALCULADORA: " << endl;
+    cout << "Introduzca los números: " << endl;
     cin >> x >> y;
-    cin >> c;
-    switch(c)
+    do
     {
-        case 's':
-        cout << "SUMA: " << x  << " + " << y << " = " << x+y << endl;
-        break;
-        case 'r':
-        cout << "RESTA: " << x << " - " << y << " = " << x-y << endl;   
-        break;
-        case 'm':
-        cout << "MULTIPLICACIÓN: " << x << " * " << y << " = " << x*y << endl;
-        break;
-        case 'd':
-        cout << "DIVISIÓN: " << x << " / " << y << " = " << x/y << endl;
-        break;
-    }
+        cout << "Elija una opción: ";
+        cin >> c;
+        switch(c)
+        {
+            case 's':
+            cout << "SUMA: " << x  << " + " << y << " = " << x+y << endl;
+            break;
+            case 'r':
+            cout << "RESTA: " << x << " - " << y << " = " << x-y << endl;   
+            break;
+            case 'm':
+            cout << "MULTIPLICACIÓN: " << x << " * " << y << " = " << x*y << endl;
+            break;
+            case 'd':
+            cout << "DIVISIÓN: " << x << " / " << y << " = " << x/y << endl;
+            break;
+        }
+    } while (c == 's' || c == 'r' || c == 'm' || c == 'd');
 }
